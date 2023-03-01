872983e21a73d7d352c7369a6f80e45b107e6030
青袍君 <1455670697@qq.com>
2023-03-01 09:33:39 +0800

    time picker

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,11 @@ Component({
     },
 
     data: {
+        start:'2022-01',
+        end:'2022-12',
+        mode: '',
+        second: '10:00:00',
+        minute: '23:59',
         activeValues: [],
         value: 'predict',
         list: [{
@@ -28,6 +33,44 @@ Component({
             this.setData({
                 activeValues: e.detail.value,
             });
+        },
+        showPicker(e) {
+            const {
+                mode
+            } = e.currentTarget.dataset;
+            this.setData({
+                mode,
+                [`${mode}Visible`]: true,
+            });
+        },
+        hidePicker() {
+            const {
+                mode
+            } = this.data;
+            this.setData({
+                [`${mode}Visible`]: false,
+            });
+        },
+        onConfirm(e) {
+            const {
+                value
+            } = e.detail;
+            const {
+                mode
+            } = this.data;
+
+            console.log('confim', value);
+
+            this.setData({
+                [mode]: value,
+                [`${mode}Text`]: value,
+            });
+
+            this.hidePicker();
+        },
+
+        onColumnChange(e) {
+            console.log('pick', e.detail.value);
         },
     },
 });
