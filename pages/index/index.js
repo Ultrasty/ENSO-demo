86b6ed0c784be621a6d3dc8b9959ea8c09e1bc74
青袍君 <1455670697@qq.com>
2023-03-03 20:13:53 +0800

    懒加载成功，保存来之不易的版本！

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,11 @@
-import {
-    initChart,
-    initChartRotated
-} from '../../utils/mychart.js'
 
 Component({
-
     options: {
         styleIsolation: 'apply-shared',
     },
 
     data: {
+        // visible:true,
         cur: {},
         position: [{
                 value: 'top',
@@ -53,7 +49,7 @@ Component({
             onInit: initChart
         },
         ecRotated: {
-            onInit: initChartRotated
+            lazyLoad: true
         }
     },
 
@@ -120,6 +116,13 @@ Component({
                     });
                 },
             );
+            
+            setTimeout(()=>{
+                this.ecComponent = this.selectComponent('#mychart-dom-bar2');
+                this.ecComponent.init(initChartRotated);
+            },1000)
+            
+            
         },
         onVisibleChange(e) {
             this.setData({
@@ -127,9 +130,163 @@ Component({
             });
         },
         onClose() {
+            
+            // var options = chartRotated.getOption();
+            // chartRotated.clear();
+            // chartRotated.setOption(options);
             this.setData({
                 visible: false,
             });
         },
+        test(){
+            var options = chart.getOption();
+            chart.clear();
+            chart.setOption(options);
+        }
     },
 });
+
+import * as echarts from '../../ec-canvas/echarts';
+
+let chart = null;
+let chartRotated = null;
+
+function initChart(canvas, width, height, dpr) {
+    chart = echarts.init(canvas, null, {
+        width: width,
+        height: height,
+        devicePixelRatio: dpr // 像素
+    });
+    canvas.setChart(chart);
+
+    var option = {
+        grid:{
+            y:35
+        },
+        dataZoom: [
+            // 本来是用来水平移动图表的，但是有概率会闪退，遂放弃
+            // {
+            //     start: 0,
+            //     end: 50,
+            //     filterMode: 'none'
+            // },
+            // {
+            //     type: 'inside',
+            //     start: 0,
+            //     end: 50
+            // }
+        ],
+        title: {
+            text: 'Niño 3.4 Forecast Results',
+            left: 'center',
+            textStyle: {
+                fontSize:12
+            }
+        },
+        xAxis: {
+            data: ["Oct-22", "Nov-22", "Dec-22", "Jan-23", "Feb-23", "Mar-23", "Apr-23", "May-23", "Jun-23", "Jul-23", "Aug-23", "Sep-23", "Oct-23", "Nov-23", "Dec-23", "Jan-24", "Feb-24"]
+        },
+        yAxis: {
+            // min:-3,
+            // max:3,
+            splitLine: {
+                lineStyle: {
+                  type: 'dashed'
+                }
+            }
+        },
+        legend: {
+            data: ['ENSO-Cross', 'ENSO-ASC', 'ENSO-MC','EnsembleForecast'],
+            bottom:0
+        },
+        series: [{
+                name:'ENSO-Cross',
+                data: [-0.85, -0.74, -0.60, -0.46, -0.30, -0.19, -0.14, 0.19, 0.59, 0.88, 1.11, 1.27, 1.34, 1.45, 1.55, 1.57, 1.43],
+                type: 'line',
+            },
+            {
+                name:'ENSO-ASC',
+                data: [-0.98, -0.67, -0.74, -0.93, -0.35, 0.33, 0.42, 0.48, 0.19, -0.25, -0.49, -0.71, -0.35, 0.05, 0.33, 1.41, 1.28],
+                type: 'line',
+            }, {
+                name:'ENSO-MC',
+                data: [-1.17, -0.96, -0.95, -0.96, -0.61, -0.23, -0.20, -0.10, -0.23, -0.28, -0.28, -0.28, -0.06, 0.20, 0.37, 0.93, 0.80],
+                type: 'line',
+            }, {
+                name:'EnsembleForecast',
+                data: [-0.92, -0.70, -0.67, -0.69, -0.33, 0.07, 0.14, 0.34, 0.39, 0.32, 0.31, 0.28, 0.49, 0.75, 0.94, 1.49, 1.36],
+                type: 'line',
+            }
+        ]
+    };
+    chart.setOption(option);
+    return chart;
+}
+
+
+function initChartRotated(canvas, width, height, dpr) {
+    chartRotated = echarts.init(canvas, null, {
+        width: width,
+        height: height,
+        devicePixelRatio: dpr // 像素
+    });
+    canvas.setChart(chartRotated);
+
+    var option = {
+        grid:{
+            // y:35
+        },
+        title: {
+            text: 'Niño 3.4 Forecast Results',
+            left: 'center',
+            textStyle: {
+                fontSize:12
+            }
+        },
+        yAxis: {
+            axisLabel :{
+                rotate : -90
+            },
+            inverse :'true',
+            data: ["Oct-22", "Nov-22", "Dec-22", "Jan-23", "Feb-23", "Mar-23", "Apr-23", "May-23", "Jun-23", "Jul-23", "Aug-23", "Sep-23", "Oct-23", "Nov-23", "Dec-23", "Jan-24", "Feb-24"]
+        },
+        xAxis: {
+            // min:-3,
+            // max:3,
+            axisLabel :{  //坐标轴刻度标签的相关设置。
+                rotate : 90 //刻度标签旋转的角度，
+            },
+            position :'top',
+            splitLine: {
+                lineStyle: {
+                  type: 'dashed'
+                }
+            }
+        },
+        legend: {
+            data: ['ENSO-Cross', 'ENSO-ASC', 'ENSO-MC','EnsembleForecast'],
+            bottom:0
+        },
+        series: [{
+                name:'ENSO-Cross',
+                data: [-0.85, -0.74, -0.60, -0.46, -0.30, -0.19, -0.14, 0.19, 0.59, 0.88, 1.11, 1.27, 1.34, 1.45, 1.55, 1.57, 1.43],
+                type: 'line',
+            },
+            {
+                name:'ENSO-ASC',
+                data: [-0.98, -0.67, -0.74, -0.93, -0.35, 0.33, 0.42, 0.48, 0.19, -0.25, -0.49, -0.71, -0.35, 0.05, 0.33, 1.41, 1.28],
+                type: 'line',
+            }, {
+                name:'ENSO-MC',
+                data: [-1.17, -0.96, -0.95, -0.96, -0.61, -0.23, -0.20, -0.10, -0.23, -0.28, -0.28, -0.28, -0.06, 0.20, 0.37, 0.93, 0.80],
+                type: 'line',
+            }, {
+                name:'EnsembleForecast',
+                data: [-0.92, -0.70, -0.67, -0.69, -0.33, 0.07, 0.14, 0.34, 0.39, 0.32, 0.31, 0.28, 0.49, 0.75, 0.94, 1.49, 1.36],
+                type: 'line',
+            }
+        ]
+    };
+    chartRotated.setOption(option);
+    return chartRotated;
+}
