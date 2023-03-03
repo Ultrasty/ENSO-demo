6f35810d0f7706ff07abfdbdee47df202aa0a990
青袍君 <1455670697@qq.com>
2023-03-03 12:43:11 +0800

    增加了popup

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,158 +1,7 @@
-import * as echarts from '../../ec-canvas/echarts';
-
-let chart = null;
-
-function initChart(canvas, width, height, dpr) {
-    const chart = echarts.init(canvas, null, {
-        width: width,
-        height: height,
-        devicePixelRatio: dpr // 像素
-    });
-    canvas.setChart(chart);
-
-    var option = {
-        grid:{
-            y:35
-        },
-        dataZoom: [
-            // 本来是用来水平移动图表的，但是有概率会闪退，遂放弃
-            // {
-            //     start: 0,
-            //     end: 50,
-            //     filterMode: 'none'
-            // },
-            // {
-            //     type: 'inside',
-            //     start: 0,
-            //     end: 50
-            // }
-        ],
-        title: {
-            text: 'Niño 3.4 Forecast Results',
-            left: 'center',
-            textStyle: {
-                fontSize:12
-            }
-        },
-        xAxis: {
-            data: ["Oct-22", "Nov-22", "Dec-22", "Jan-23", "Feb-23", "Mar-23", "Apr-23", "May-23", "Jun-23", "Jul-23", "Aug-23", "Sep-23", "Oct-23", "Nov-23", "Dec-23", "Jan-24", "Feb-24"]
-        },
-        yAxis: {
-            // min:-3,
-            // max:3,
-            splitLine: {
-                lineStyle: {
-                  type: 'dashed'
-                }
-            }
-        },
-        legend: {
-            data: ['ENSO-Cross', 'ENSO-ASC', 'ENSO-MC','EnsembleForecast'],
-            bottom:0
-        },
-        series: [{
-                name:'ENSO-Cross',
-                data: [-0.85, -0.74, -0.60, -0.46, -0.30, -0.19, -0.14, 0.19, 0.59, 0.88, 1.11, 1.27, 1.34, 1.45, 1.55, 1.57, 1.43],
-                type: 'line',
-            },
-            {
-                name:'ENSO-ASC',
-                data: [-0.98, -0.67, -0.74, -0.93, -0.35, 0.33, 0.42, 0.48, 0.19, -0.25, -0.49, -0.71, -0.35, 0.05, 0.33, 1.41, 1.28],
-                type: 'line',
-            }, {
-                name:'ENSO-MC',
-                data: [-1.17, -0.96, -0.95, -0.96, -0.61, -0.23, -0.20, -0.10, -0.23, -0.28, -0.28, -0.28, -0.06, 0.20, 0.37, 0.93, 0.80],
-                type: 'line',
-            }, {
-                name:'EnsembleForecast',
-                data: [-0.92, -0.70, -0.67, -0.69, -0.33, 0.07, 0.14, 0.34, 0.39, 0.32, 0.31, 0.28, 0.49, 0.75, 0.94, 1.49, 1.36],
-                type: 'line',
-            }
-        ]
-    };
-    chart.setOption(option);
-    return chart;
-}
-
-function initChartRotated(canvas, width, height, dpr) {
-    const chart = echarts.init(canvas, null, {
-        width: width,
-        height: height,
-        devicePixelRatio: dpr // 像素
-    });
-    canvas.setChart(chart);
-
-    var option = {
-        grid:{
-            // y:35
-        },
-        dataZoom: [
-            // 本来是用来水平移动图表的，但是有概率会闪退，遂放弃
-            // {
-            //     start: 0,
-            //     end: 50,
-            //     filterMode: 'none'
-            // },
-            // {
-            //     type: 'inside',
-            //     start: 0,
-            //     end: 50
-            // }
-        ],
-        title: {
-            text: 'Niño 3.4 Forecast Results',
-            left: 'center',
-            textStyle: {
-                fontSize:12
-            }
-        },
-        yAxis: {
-            axisLabel :{
-                rotate : -90
-            },
-            inverse :'true',
-            data: ["Oct-22", "Nov-22", "Dec-22", "Jan-23", "Feb-23", "Mar-23", "Apr-23", "May-23", "Jun-23", "Jul-23", "Aug-23", "Sep-23", "Oct-23", "Nov-23", "Dec-23", "Jan-24", "Feb-24"]
-        },
-        xAxis: {
-            // min:-3,
-            // max:3,
-            axisLabel :{  //坐标轴刻度标签的相关设置。
-                rotate : 90 //刻度标签旋转的角度，
-            },
-            position :'top',
-            splitLine: {
-                lineStyle: {
-                  type: 'dashed'
-                }
-            }
-        },
-        legend: {
-            data: ['ENSO-Cross', 'ENSO-ASC', 'ENSO-MC','EnsembleForecast'],
-            bottom:0
-        },
-        series: [{
-                name:'ENSO-Cross',
-                data: [-0.85, -0.74, -0.60, -0.46, -0.30, -0.19, -0.14, 0.19, 0.59, 0.88, 1.11, 1.27, 1.34, 1.45, 1.55, 1.57, 1.43],
-                type: 'line',
-            },
-            {
-                name:'ENSO-ASC',
-                data: [-0.98, -0.67, -0.74, -0.93, -0.35, 0.33, 0.42, 0.48, 0.19, -0.25, -0.49, -0.71, -0.35, 0.05, 0.33, 1.41, 1.28],
-                type: 'line',
-            }, {
-                name:'ENSO-MC',
-                data: [-1.17, -0.96, -0.95, -0.96, -0.61, -0.23, -0.20, -0.10, -0.23, -0.28, -0.28, -0.28, -0.06, 0.20, 0.37, 0.93, 0.80],
-                type: 'line',
-            }, {
-                name:'EnsembleForecast',
-                data: [-0.92, -0.70, -0.67, -0.69, -0.33, 0.07, 0.14, 0.34, 0.39, 0.32, 0.31, 0.28, 0.49, 0.75, 0.94, 1.49, 1.36],
-                type: 'line',
-            }
-        ]
-    };
-    chart.setOption(option);
-    return chart;
-}
+import {
+    initChart,
+    initChartRotated
+} from '../../utils/mychart.js'
 
 Component({
 
@@ -161,6 +10,28 @@ Component({
     },
 
     data: {
+        cur: {},
+        position: [{
+                value: 'top',
+                text: '顶部弹出'
+            },
+            {
+                value: 'left',
+                text: '左侧弹出'
+            },
+            {
+                value: 'center',
+                text: '中间弹出'
+            },
+            {
+                value: 'bottom',
+                text: '底部弹出'
+            },
+            {
+                value: 'right',
+                text: '右侧弹出'
+            },
+        ],
         isPC: wx.getSystemInfoSync().platform == "devtools",
         start: '2022-01',
         end: '2022-12',
@@ -234,6 +105,31 @@ Component({
 
         onColumnChange(e) {
             console.log('pick', e.detail.value);
+        },
+        handlePopup(e) {
+            const {
+                item
+            } = e.currentTarget.dataset;
+
+            this.setData({
+                    cur: item,
+                },
+                () => {
+                    this.setData({
+                        visible: true
+                    });
+                },
+            );
+        },
+        onVisibleChange(e) {
+            this.setData({
+                visible: e.detail.visible,
+            });
+        },
+        onClose() {
+            this.setData({
+                visible: false,
+            });
         },
     },
 });
