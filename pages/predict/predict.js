cbc0bc728224623661fcb892f4e68919755fdea0
青袍君 <1455670697@qq.com>
2023-03-05 16:57:34 +0800

    修改了回调函数

--- pages/predict/predict.js
+++ pages/predict/predict.js
@@ -197,19 +197,19 @@ Component({
         //转发、分享到朋友圈、收藏时的回调函数
         onShareAppMessage: function (options) {
             return {
-                title: 'ENSO预测与分析',
+                title: '同济大学ENSO预测与分析',
                 imageUrl: '/imgs/nesdis-el-nino.jpg',
             }
         },
         onShareTimeline: function () {
             return {
-                title: 'ENSO预测与分析',
+                title: '同济大学ENSO预测与分析',
                 imageUrl: '/imgs/nesdis-el-nino.jpg',
             }
         },
         onAddToFavorites: function (options) {
             return {
-                title: 'ENSO预测与分析',
+                title: '同济大学ENSO预测与分析',
                 imageUrl: '/imgs/nesdis-el-nino.jpg',
             }
         },
