09be940ee4887b38b5e89945039c0104b05fec44
青袍君 <1455670697@qq.com>
2023-03-05 20:06:03 +0800

    analyse页 initchart改造成功，接下来重构一下predict页

--- pages/analyse/analyse.js
+++ pages/analyse/analyse.js
@@ -46,11 +46,51 @@ Component({
         },
         test() {
             let ecComponent = this.selectComponent("#chart4");
-            ecComponent.init(initChart,7);
+            ecComponent.init(this.initChart);
 
             // wx.navigateTo({
             //   url: '/pages/nullpage/nullpage',
             // })
+        },
+        initChart(canvas, width, height, dpr,test) {
+            console.log(test)
+            chart4 = echarts.init(canvas, null, {
+                width: width,
+                height: height,
+                devicePixelRatio: dpr // 像素
+            });
+            canvas.setChart(chart4);
+        
+            var option = {
+                legend: {
+                    // Try 'horizontal'
+                    orient: 'vertical',
+                    right: 10,
+                    top: 'center'
+                },
+                dataset: {
+                    source: [
+                        ['product', '2015', '2016', '2017'],
+                        ['Matcha Latte', 43.3, 85.8, 93.7],
+                        ['Milk Tea', 83.1, 73.4, 55.1],
+                        ['Cheese Cocoa', 86.4, 65.2, 82.5],
+                        ['Walnut Brownie', 72.4, 53.9, 39.1]
+                    ]
+                },
+                xAxis: {
+                    type: 'category'
+                },
+                yAxis: {},
+                series: [{
+                    type: 'bar'
+                }, {
+                    type: 'bar'
+                }, {
+                    type: 'bar'
+                }]
+            };
+            chart4.setOption(option);
+            return chart4;
         }
     }
 })
@@ -59,44 +99,3 @@ import * as echarts from '../../ec-canvas/echarts';
 
 //保存图表实例
 let chart4 = null;
-
-function initChart(canvas, width, height, dpr,test) {
-    console.log(test)
-    chart4 = echarts.init(canvas, null, {
-        width: width,
-        height: height,
-        devicePixelRatio: dpr // 像素
-    });
-    canvas.setChart(chart4);
-
-    var option = {
-        legend: {
-            // Try 'horizontal'
-            orient: 'vertical',
-            right: 10,
-            top: 'center'
-        },
-        dataset: {
-            source: [
-                ['product', '2015', '2016', '2017'],
-                ['Matcha Latte', 43.3, 85.8, 93.7],
-                ['Milk Tea', 83.1, 73.4, 55.1],
-                ['Cheese Cocoa', 86.4, 65.2, 82.5],
-                ['Walnut Brownie', 72.4, 53.9, 39.1]
-            ]
-        },
-        xAxis: {
-            type: 'category'
-        },
-        yAxis: {},
-        series: [{
-            type: 'bar'
-        }, {
-            type: 'bar'
-        }, {
-            type: 'bar'
-        }]
-    };
-    chart4.setOption(option);
-    return chart4;
-}
